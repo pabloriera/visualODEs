3ca0f0178bf4774e4f9c89b8a66c12394562502f
gcorallo <g@gcrll.net>
2013-07-28 15:11:01 -0300

    visual class add

---
+++ src/Visual.cpp
@@ -0,0 +1,9 @@
+//
+//  Visual.cpp
+//  visualODEs
+//
+//  Created by Guido c on 7/28/13.
+//
+//
+
+#include "Visual.h"
---
+++ src/Visual.h
@@ -0,0 +1,14 @@
+//
+//  Visual.h
+//  visualODEs
+//
+//  Created by Guido c on 7/28/13.
+//
+//
+
+#ifndef visualODEs_Visual_h
+#define visualODEs_Visual_h
+
+
+
+#endif
